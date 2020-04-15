eb476becd303ee4e1888e0641671b13432a274ff
ruchika <ruchika6698@gmail.com>
2020-04-15 17:52:04 +0530

    Added:Read File and add information into file using opencsv

---
+++ People.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OpenCSVFile
+{
+    class People
+    {
+        /// <summary>
+        /// Main method read the data from file
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            string filepath = @"C:\Users\boss\source\repos\OpenCSV\People.csv";
+            List<Person> people = new List<Person>();
+            List<string> lines = File.ReadAllLines(filepath).ToList();
+
+            foreach (string line in lines)
+            {
+                string[] entries = line.Split(',');
+                Person newPerson = new Person();
+
+                newPerson.Name = entries[0];
+                newPerson.EmailAddress = entries[1];
+                newPerson.PhoneNumber = entries[2];
+                newPerson.Country = entries[3];
+
+                people.Add(newPerson);
+            }
+
+            foreach (var person in people)
+            {
+                Console.WriteLine($"{ person.Name } ,{ person.EmailAddress } , { person.PhoneNumber }  ,{ person.Country }");
+            }
+            //New Data added in file
+            Console.WriteLine(" ");
+            people.Add(new Person { Name = "Rajesh Sharma", EmailAddress = "rajesh@gmail.com", PhoneNumber = "+91-9128933564", Country = "India" });
+            List<string> output = new List<string>();
+            foreach (var person in people)
+            {
+                output.Add($"{ person.Name }, { person.EmailAddress } , { person.PhoneNumber } , { person.Country }");
+            }
+            Console.WriteLine("Person details added");
+            Console.WriteLine("Writing into the file");
+            File.WriteAllLines(filepath, output);
+            foreach (var person in people)
+            {
+                Console.WriteLine($"{ person.Name } ,{ person.EmailAddress }, { person.PhoneNumber } , { person.Country }");
+            }
+        }
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenCSVFile
+{
+    /// <summary>
+    /// Read the Name ,Email Id,Phone number,Country
+    /// </summary>
+   public class Person
+   {
+        public string Name { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Country { get; set; }
+    }
+}
